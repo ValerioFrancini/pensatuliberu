b9028363f2af41051cbde61821fd0495e66d009c
valerio.hostairbnb <valerio.hostairbnb@gmail.com>
2024-12-13 00:50:05 +0000

    .

--- style.js
+++ style.js
@@ -1,13 +1,11 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
-    const content = document.getElementById('content');
-    const errorMessage = document.getElementById('error-message'); // Nuovo elemento per il messaggio di errore
+    const errorMessage = document.getElementById('error-message');
 
     if (password === 'tvb') {
-        content.style.display = 'block';
-        errorMessage.style.display = 'none'; // Nascondi il messaggio di errore
+        // Reindirizza alla pagina app.html
+        window.location.href = 'app.html';
     } else {
-        content.style.display = 'none';
-        errorMessage.style.display = 'block'; // Mostra il messaggio di errore
+        errorMessage.textContent = 'Password errata';
     }
 }
