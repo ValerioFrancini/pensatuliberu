04f1bd96c1cc92a7b35d06a46ea8393a912fad13
valerio.hostairbnb <valerio.hostairbnb@gmail.com>
2024-12-13 00:47:29 +0000

    ,

--- style.js
+++ style.js
@@ -1,10 +1,13 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const content = document.getElementById('content');
+    const errorMessage = document.getElementById('error-message'); // Nuovo elemento per il messaggio di errore
 
     if (password === 'tvb') {
         content.style.display = 'block';
+        errorMessage.style.display = 'none'; // Nascondi il messaggio di errore
     } else {
-        alert('scemottu');
+        content.style.display = 'none';
+        errorMessage.style.display = 'block'; // Mostra il messaggio di errore
     }
 }
