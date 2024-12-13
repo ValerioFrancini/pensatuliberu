014444fd7a901f1cc5a60299c99c65dccdd4dbb9
valerio.hostairbnb <valerio.hostairbnb@gmail.com>
2024-12-13 10:14:00 +0000

    .

--- importfirebase.js
+++ importfirebase.js
@@ -1,24 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDv0J1YBf54mLZW_Q9kVwBMRRzBLPqjesI",
-    authDomain: "pensatuliberu.firebaseapp.com",
-    projectId: "pensatuliberu",
-    storageBucket: "pensatuliberu.firebasestorage.app",
-    messagingSenderId: "400150403662",
-    appId: "1:400150403662:web:2ee3e68af31daa120a811f",
-    measurementId: "G-VPQWGBTJBG"
-  };
+  apiKey: "AIzaSyDv0J1YBf54mLZW_Q9kVwBMRRzBLPqjesI",
+  authDomain: "pensatuliberu.firebaseapp.com",
+  projectId: "pensatuliberu",
+  storageBucket: "pensatuliberu.firebasestorage.app",
+  messagingSenderId: "400150403662",
+  appId: "1:400150403662:web:2ee3e68af31daa120a811f",
+  measurementId: "G-VPQWGBTJBG"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-}
+const analytics = getAnalytics(app);
