c71b8163d3e75e5976451eac593a634170d9cfc1
valerio.hostairbnb <valerio.hostairbnb@gmail.com>
2024-12-13 09:07:09 +0000

    .

---
+++ firebase.js
@@ -0,0 +1,25 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/
+setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDv0J1YBf54mLZW_Q9kVwBMRRzBLPqjesI",
+  authDomain: "pensatuliberu.firebaseapp.com",
+  projectId: "pensatuliberu",
+  storageBucket: "pensatuliberu.firebasestorage.app",
+  messagingSenderId: "400150403662",
+  appId: "1:400150403662:web:2ee3e68af31daa120a811f",
+  measurementId: "G-VPQWGBTJBG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
